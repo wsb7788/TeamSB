21a268838947fee143fb4233c03da692ba927e65
wsb7788 <wsb0514@naver.com>
2021-07-02 01:27:12 +0900

    start git!

---
+++ app/src/main/java/com/project/teamsb/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.project.teamsb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.project.teamsb.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button1.setOnClickListener {  }
+
+
+    }
+}
