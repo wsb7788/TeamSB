a48ea3f4208eeef775a854bf2e190bc69eb01f14
wsb7788 <wsb0514@naver.com>
2021-07-14 00:18:33 +0900

    메뉴 버튼 이미지설정

--- app/src/main/java/com/project/teamsb/HomeFragment.kt
+++ app/src/main/java/com/project/teamsb/HomeFragment.kt
@@ -34,7 +34,7 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_percent, container, false)
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
     companion object {
