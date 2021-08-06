12403dfed5d35142333e472826b85fc4a8764671
wsb7788 <wsb0514@naver.com>
2021-08-07 06:36:18 +0900

    파이어베이스(포그라운드)알림 구현

--- app/src/main/java/com/project/teamsb/api/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/project/teamsb/api/MyFirebaseMessagingService.kt
@@ -1,5 +1,6 @@
 package com.project.teamsb.api
 
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -10,6 +11,7 @@ import android.os.Build
 import android.util.Log
 import android.widget.Toast
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingService
@@ -17,9 +19,11 @@ import com.google.firebase.messaging.RemoteMessage
 import com.project.teamsb.R
 import com.project.teamsb.login.LoginActivity
 import com.project.teamsb.main.MainActivity
+import com.project.teamsb.toolbar.AlertActivity
 
 class MyFirebaseMessagingService:FirebaseMessagingService() {
     private val TAG: String = this.javaClass.simpleName
+    lateinit var managerCompat: NotificationManagerCompat
 
     override fun onMessageReceived(remoteMessage: RemoteMessage)
     {
@@ -47,6 +51,7 @@ class MyFirebaseMessagingService:FirebaseMessagingService() {
     }
 
     // 받은 알림을 기기에 표시하는 메서드
+    @SuppressLint("ServiceCast")
     private fun sendNotification(title:String, body:String)
     {
 
@@ -57,15 +62,17 @@ class MyFirebaseMessagingService:FirebaseMessagingService() {
 
         val channelId = "my_channel"
         val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+        try{
+        val notificationBuilder = NotificationCompat.Builder(this,channelId)
             .setContentTitle(title)
             .setContentText(body)
             .setAutoCancel(true)
+            .setSmallIcon(R.drawable.logo)
             .setSound(defaultSoundUri)
             .setContentIntent(pendingIntent)
 
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
+        //val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManagerCompat
+        val notificationManager = NotificationManagerCompat.from(this)
         // 오레오 버전 예외처리
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(channelId,
@@ -75,6 +82,11 @@ class MyFirebaseMessagingService:FirebaseMessagingService() {
         }
 
         notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
+
+        }
+        catch(e: Exception){
+            e.printStackTrace();
+        }
     }
 
 
