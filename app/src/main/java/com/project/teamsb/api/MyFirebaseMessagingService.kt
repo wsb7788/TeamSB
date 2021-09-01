ec0721f8d16651933be82ce294c142085369588e
wsb7788 <wsb0514@naver.com>
2021-09-02 02:05:21 +0900

    노티 안사라지는 버그 수정

--- app/src/main/java/com/project/teamsb/api/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/project/teamsb/api/MyFirebaseMessagingService.kt
@@ -36,14 +36,15 @@ class MyFirebaseMessagingService:FirebaseMessagingService() {
     private val TAG: String = this.javaClass.simpleName
     lateinit var managerCompat: NotificationManagerCompat
 
+    var channel_id = 0
     override fun onMessageReceived(remoteMessage: RemoteMessage)
     {
 
 
-        if(remoteMessage.data.isNotEmpty()){
+    /*    if(remoteMessage.data.isNotEmpty()){
             sendNotification(remoteMessage.data!!["title"]!!, remoteMessage.data["body"]!!)
 
-        }
+        }*/
         if (remoteMessage.notification == null)
         {
             //sendNotification(remoteMessage.notification!!.title!!, remoteMessage.notification!!.body!!)
@@ -80,19 +81,16 @@ class MyFirebaseMessagingService:FirebaseMessagingService() {
         try{
 
 
-
-
             val notificationBuilder = NotificationCompat.Builder(App.instance, channelId)
                 .setContentTitle(title)
                 .setContentText(body)
                 .setStyle(NotificationCompat.BigTextStyle().bigText(body))
                 .setAutoCancel(true)
-                .setSmallIcon(R.drawable.ic_stat_name)
+                .setSmallIcon(R.drawable.noti_icon)
                 .setLargeIcon(micon)
-                .setColor(Color.parseColor("#ff0000"))
+                //.setColor(Color.parseColor("#ff0000"))
                 .setSound(defaultSoundUri)
                 .setContentIntent(pendingIntent)
-                .setFullScreenIntent(pendingIntent,true)                //위에 띄우기
                 .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)            //잠금화면에서 보이기
                 .setCategory(NotificationCompat.CATEGORY_SOCIAL)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
@@ -121,8 +119,9 @@ class MyFirebaseMessagingService:FirebaseMessagingService() {
                 // 오레오 버전 예외처리
 
             }
-        notificationManager.notify(random /* ID of notification */, notificationBuilder.build())
-
+        notificationManager.notify(channel_id /* ID of notification */, notificationBuilder.build())
+        channel_id++
+            if(channel_id == 100) channel_id = 0
         }
         catch(e: Exception){
             e.printStackTrace();
